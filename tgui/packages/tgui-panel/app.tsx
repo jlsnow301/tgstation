afc2ccf596d5b0773993989402c7753c66d591c9
jlsnow301 <jlsnow301@pm.me>
2025-12-15 03:35:25 -0800

    ok

--- tgui/packages/tgui-panel/app.tsx
+++ tgui/packages/tgui-panel/app.tsx
@@ -1,6 +1,35 @@
 import { Provider } from 'jotai';
+import { captureExternalLinks } from 'tgui/links';
+import { EventBus } from 'tgui-core/eventbus';
+import { setupGlobalEvents } from 'tgui-core/events';
+import { listeners } from './events/listeners';
 import { store } from './events/store';
 import { Panel } from './Panel';
+import { setupPanelFocusHacks } from './panelFocus';
+
+const bus = new EventBus(listeners);
+
+setupGlobalEvents({
+  ignoreWindowFocus: true,
+});
+
+setupPanelFocusHacks();
+captureExternalLinks();
+
+// Dispatch incoming messages as store actions
+Byond.subscribe((type, payload) => bus.dispatch({ type, payload }));
+
+// Unhide the panel
+Byond.winset('output_selector.legacy_output_selector', {
+  left: 'output_browser',
+});
+
+// Resize the panel to match the non-browser output
+Byond.winget('output').then((output: { size: string }) => {
+  Byond.winset('browseroutput', {
+    size: output.size,
+  });
+});
 
 /** Just an expandable wrapper for setup shenanigans and providers */
 export function App() {
--- tgui/packages/tgui-panel/index.tsx
+++ tgui/packages/tgui-panel/index.tsx
@@ -4,68 +4,14 @@
  * @license MIT
  */
 
-// Themes
 import './styles/main.scss';
 import './styles/themes/light.scss';
-import { perf } from 'common/perf';
-import { setGlobalStore } from 'tgui/backend';
-import { captureExternalLinks } from 'tgui/links';
-import { render } from 'tgui/renderer';
-import { configureStore } from 'tgui/store';
-import { EventBus } from 'tgui-core/eventbus';
-import { setupGlobalEvents } from 'tgui-core/events';
-import { setupHotReloading } from 'tgui-dev-server/link/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { App } from './app';
-import { listeners } from './events/listeners';
-import { setupPanelFocusHacks } from './panelFocus';
 
-perf.mark('inception', window.performance?.timeOrigin);
-perf.mark('init');
-
-const bus = new EventBus(listeners);
-const store = configureStore();
-
-function setupApp() {
-  // Delay setup
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', setupApp);
-    return;
-  }
-
-  setGlobalStore(store);
-
-  setupGlobalEvents({
-    ignoreWindowFocus: true,
-  });
-
-  setupPanelFocusHacks();
-  captureExternalLinks();
-
-  // Dispatch incoming messages as store actions
-  Byond.subscribe((type, payload) => bus.dispatch({ type, payload }));
-
-  // Unhide the panel
-  Byond.winset('output_selector.legacy_output_selector', {
-    left: 'output_browser',
-  });
-
-  // Resize the panel to match the non-browser output
-  Byond.winget('output').then((output: { size: string }) => {
-    Byond.winset('browseroutput', {
-      size: output.size,
-    });
-  });
-
-  store.subscribe(() => render(<App />));
-
-  // Enable hot module reloading
-  if (import.meta.webpackHot) {
-    setupHotReloading();
-
-    import.meta.webpackHot.accept(['./Notifications', './Panel'], () => {
-      render(<App />);
-    });
-  }
-}
-
-setupApp();
+createRoot(document.getElementById('react-root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
